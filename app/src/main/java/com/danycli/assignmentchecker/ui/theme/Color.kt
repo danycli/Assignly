2e663b60a16cd2729fac4449cc7bec42996b1e1a
danycli <angelgaming331@gmail.com>
2026-04-22 16:32:49 +0500

    solve the solved assignment number issue solve the screen tilting issue

---
+++ app/src/main/java/com/danycli/assignmentchecker/ui/theme/Color.kt
@@ -0,0 +1,13 @@
+package com.danycli.assignmentchecker.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Cyprus = Color(0xFF004643)
+val Sand = Color(0xFFF0EDE5)
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF000000)
+val GrayText = Color(0xFF757575)
+val LightSand = Color(0xFFF7F5F0)
+val ErrorRed = Color(0xFFB00020)
+val CyprusDark = Color(0xFF003331)
+val CyprusLight = Color(0xFF005D59)
